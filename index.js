497b3a0c37d5ef040ef5e1aae2b80ecdfed7283b
Yago Garcia <Yago.Garcia@la.logicalis.com>
2019-03-14 16:52:49 -0300

    pausando projeto

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 const { DadosAmbientais } = require('./simuladores/ambientais')
 require('./startup/routes')(app);
 
-DadosAmbientais()
+//DadosAmbientais()
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Escutando porta ${port}...`));
